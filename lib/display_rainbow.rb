decf9faad7d246ece9174d2e36f96a6a276295e9
Thierry Uwilingiyimana <thierry.uwi@gmail.com>
2019-01-23 19:31:17 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,18 +1,10 @@
-# Write your #display_rainbow method here
-
 def display_rainbow(colors)
   # I cannot seem to get back the error setup by these tests:
   # expect(colors).to receive(:[]).with(0).at_least(:once).and_return("red")
   # what do these want?
   
-  # final_result = "colors[0].upcase"
+  puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 
-  colors.each do | color |
-    final_result << "#{color[0].upcase}: #{color}, "
-  end
-
-  puts final_result[0..final_result.length-3]
 end
 
-#colors = ["red","orange","blue"]
-#display_rainbow(colors)
+display_rainbow(["red", "orange", "yellow", "green", "blue", "indigo", "violet"])
